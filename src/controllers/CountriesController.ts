774b25c02b7ab4e5f1700973d7565d01b3beb0b3
Arash Paknezad <a.paknezad@hotmail.com>
2019-11-19 14:38:10 +0100

    feature: sort countries

--- src/controllers/CountriesController.ts
+++ src/controllers/CountriesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { controller, get } from '../decorators';
 import { RepositoryFactory } from '../factories/RepositoryFactory';
 import { ArrayUtil } from '../utils/ArrayUtil';
+import { Sort } from '../enums/Sort';
 
 const countriesApi = RepositoryFactory.getDefaultCountriesApi();
 
@@ -10,8 +11,14 @@ class CountriesController {
     @get('/')
     async getCountries(req: Request, res: Response): Promise<void> {
         const search: string = req.query.search || '';
+        const sort: string = req.query.sort || Sort.ASC;
+        const ascending = sort === Sort.ASC;
 
         let countries = await countriesApi.getAll();
+
+        if (sort) {
+            countries = ArrayUtil.sort(countries, 'name', !ascending);
+        }
 
         if (search) {
             countries = ArrayUtil.filter(countries, 'name', search);
@@ -20,6 +27,8 @@ class CountriesController {
         res.render('countries', {
             countries,
             search,
+            sort,
+            ascending,
         });
     }
 
--- src/utils/ArrayUtil.ts
+++ src/utils/ArrayUtil.ts
@@ -7,4 +7,18 @@ export class ArrayUtil {
             return value.toLowerCase().includes(search.toLowerCase());
         });
     }
+
+    static sort<T>(entities: T[], key: KeysMatching<T, string>, reverse = false): T[] {
+        const sortedEntities = entities.sort((entityA: T, entityB: T) => {
+            const entityAValue = (entityA[key] as unknown) as string;
+            const entityBValue = (entityB[key] as unknown) as string;
+            return entityAValue.localeCompare(entityBValue);
+        });
+
+        if (reverse) {
+            return sortedEntities.reverse();
+        }
+
+        return sortedEntities;
+    }
 }
